99b58f40ab31931d6864a108d88e62b4e496fa66
Ramzi Akremi <ramzi.dev@icloud.com>
2022-11-15 08:26:07 +1100

    chore: initialise repository

---
+++ config/environment.zsh
@@ -0,0 +1,29 @@
+#                 ██
+#                ░██
+#  ██████  ██████░██
+# ░░░░██  ██░░░░ ░██████
+#    ██  ░░█████ ░██░░░██
+#   ██    ░░░░░██░██  ░██
+#  ██████ ██████ ░██  ░██
+# ░░░░░░ ░░░░░░  ░░   ░░
+#
+#
+
+#█▓▒░ timestamps
+HIST_STAMPS=dd/mm/yyyy
+
+#█▓▒░ Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# #█▓▒░ paths
+# export $HOME/.local/bin:$PATH
+
+#█▓▒░ preferred editor for local and remote sessions
+export EDITOR=nvim
+export VISUAL=nvim
+
+#█▓▒░ java
+# export JAVA_HOME=`/usr/libexec/java_home -v 11`
+
+#█▓▒░ java
+export HOMEBREW_NO_AUTO_UPDATE=1
---
+++ config/prompt.zsh
@@ -0,0 +1,16 @@
+#                 ██
+#                ░██
+#  ██████  ██████░██
+# ░░░░██  ██░░░░ ░██████
+#    ██  ░░█████ ░██░░░██
+#   ██    ░░░░░██░██  ░██
+#  ██████ ██████ ░██  ░██
+# ░░░░░░ ░░░░░░  ░░   ░░
+#
+
+autoload -U promptinit; promptinit
+
+# optionally define some options
+# PURE_CMD_MAX_EXEC_TIME=10
+
+prompt pure
